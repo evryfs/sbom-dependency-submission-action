6dfab867112bc19f0c0913f1970227ecb7845e4d
David J. M. Karlsen <david@davidkarlsen.com>
2022-06-21 17:10:47 +0200

    cleanup

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,7 +1,3 @@
-import {wait} from '../src/wait'
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
 import {expect, test} from '@jest/globals'
 
 test('dummy', async () => {
